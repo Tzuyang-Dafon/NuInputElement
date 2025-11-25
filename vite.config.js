7660b3f38dc365dd25ec6a883586584678c0e67c
Tzuyang ♻️ <103104204+Tzuyang-Dafon@users.noreply.github.com>
2025-11-25 06:57:13 +0000

    Modify: 更新 vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: {
-				inswz: 'src/main.js',          // 整包入口（全部元件）
+				index: 'src/main.js',          // 整包入口（全部元件）
 				'nu-time-range': 'src/nu-time-range.js',
 				'nu-time-input': 'src/nu-time-input.js',
 				'nu-checkbox': 'src/nu-checkbox.js',
@@ -14,6 +14,8 @@ export default defineConfig({
 		rollupOptions: {
 			entryFileNames: '[name].js',
 			format: 'systemjs',
+			output: {
+			},
 		}
 	}
 });
